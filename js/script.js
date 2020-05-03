98ed70a9f9fea7192f73eb378624651bcbacc01c
Ramitsan <ramitsan@gmail.com>
2020-05-03 13:23:22 +0300

    Добавляет кнопку закрытия полноэкранного изображения и выход по ESC

--- js/script.js
+++ js/script.js
@@ -1,70 +1,108 @@
 'use strict';
-//кнопка прокрутки
-var upButton = document.querySelector('.up-button');
+(function() {
+  //кнопка прокрутки
+  var upButton = document.querySelector('.up-button');
 
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 800) {
-    upButton.classList.add('shown');
-  } else {
-    upButton.classList.remove('shown');
-  }
-});
-
-upButton.addEventListener('click', function() {
-  window.scrollTo(0, 0);
-});
-
-
-//полноэкранный просмотр изображений
-var previewList = document.querySelectorAll('.js-preview');
-var overlay;
-
-previewList.forEach(function(preview) {
-  preview.addEventListener('click', function(e) {
-    e.preventDefault();
-    openBigPicture(preview);
+  window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 800) {
+      upButton.classList.add('shown');
+    } else {
+      upButton.classList.remove('shown');
+    }
   });
-});
 
-var openBigPicture = function(preview) {
-  var coverImageElement;
+  upButton.addEventListener('click', function() {
+    window.scrollTo(0, 0);
+  });
 
-  if (preview.hasAttribute('data-preview')) {
-    coverImageElement = document.createElement('img');
-    coverImageElement.src = preview.getAttribute('data-preview');
-    coverImageElement.style.border = '10px solid #ffffff';
-  } else {
-    coverImageElement = preview.cloneNode(true);
-  }
 
-  overlay = createOverlay();
-  document.body.appendChild(overlay);
+  //полноэкранный просмотр изображений
+  var previewList = document.querySelectorAll('.js-preview');
+  var overlay;
+  var buttonClose;
+  var ESC_KEYCODE = 27;
 
-  coverImageElement.onload = function() {
-    overlay.appendChild(coverImageElement);
+  previewList.forEach(function(preview) {
+    preview.addEventListener('click', function(e) {
+      e.preventDefault();
+      openBigPicture(preview);
+    });
+  });
+
+  var openBigPicture = function(preview) {
+    var coverImageElement;
+
+    if (preview.hasAttribute('data-preview')) {
+      coverImageElement = document.createElement('img');
+      coverImageElement.src = preview.getAttribute('data-preview');
+      coverImageElement.style.border = '10px solid #ffffff';
+    } else {
+      coverImageElement = preview.cloneNode(true);
+    }
+
+    overlay = createOverlay();
+    buttonClose = createButtonClose();
+    document.body.appendChild(overlay);
+    document.body.appendChild(buttonClose);
+
+    coverImageElement.onload = function() {
+      overlay.appendChild(coverImageElement)
+    };
+
+    overlay.addEventListener('click', closeBigPicture);
+    buttonClose.addEventListener('click', closeBigPicture);
   };
 
-  overlay.addEventListener('click', closeBigPicture)
-};
+  var closeBigPicture = function(e) {
+    if (e.target === overlay || e.target === buttonClose) {
+      // e.target.remove();
+      overlay.remove();
+      buttonClose.remove();
+      overlay.removeEventListener('click', closeBigPicture);
+      buttonClose.removeEventListener('click', closeBigPicture);
+    }
+  };
 
-var closeBigPicture = function(e) {
-  if (e.target === overlay) {
-    e.target.remove();
-    overlay.removeEventListener('click', closeBigPicture);
+  window.addEventListener('keydown', function(e) {
+    if (e.keyCode === ESC_KEYCODE) {
+      overlay.remove();
+      buttonClose.remove();
+      overlay.removeEventListener('click', closeBigPicture);
+      buttonClose.removeEventListener('click', closeBigPicture);
+    }
+  });
+
+  var createOverlay = function() {
+    var overlayElement = document.createElement('div');
+    overlayElement.style.position = 'fixed';
+    overlayElement.style.backgroundColor = 'rgba(0,0,0,0.8)';
+    overlayElement.style.left = 0;
+    overlayElement.style.top = 0;
+    overlayElement.style.right = 0;
+    overlayElement.style.bottom = 0;
+    overlayElement.style.zIndex = 99;
+    overlayElement.style.display = 'flex';
+    overlayElement.style.alignItems = 'center';
+    overlayElement.style.justifyContent = 'center';
+    return overlayElement;
   }
-};
 
-var createOverlay = function() {
-  var overlayElement = document.createElement('div');
-  overlayElement.style.position = 'fixed';
-  overlayElement.style.backgroundColor = 'rgba(0,0,0,0.8)';
-  overlayElement.style.left = 0;
-  overlayElement.style.top = 0;
-  overlayElement.style.right = 0;
-  overlayElement.style.bottom = 0;
-  overlayElement.style.zIndex = 99;
-  overlayElement.style.display = 'flex';
-  overlayElement.style.alignItems = 'center';
-  overlayElement.style.justifyContent = 'center';
-  return overlayElement;
-}
+  var createButtonClose = function() {
+    var buttonElement = document.createElement('button');
+    buttonElement.style.position = 'fixed';
+    buttonElement.style.backgroundColor = 'transparent';
+    buttonElement.style.right = '5%';
+    buttonElement.style.top = '5%';
+    buttonElement.style.width = '30px';
+    buttonElement.style.height = '30px';
+    buttonElement.style.display = 'block';
+    buttonElement.style.border = '1px solid transparent';
+    buttonElement.style.backgroundImage = 'url(../img/close-button.svg)';
+    buttonElement.style.backgroundPosition = 'center';
+    buttonElement.style.backgroundRepeat = 'no-repeat';
+    buttonElement.style.cursor = 'pointer';
+    buttonElement.style.zIndex = 100;
+    return buttonElement;
+  }
+
+})();
--- js/slider.js
+++ js/slider.js
@@ -1,53 +1,56 @@
 'use strict';
-// слайдер Стихи и цитаты о войне
-var slideIndex = 1;
-var slides = document.querySelectorAll('.slider__item');
-var prev = document.querySelector('.slider__button--prev');
-var next = document.querySelector('.slider__button--next');
-var sliderDotsWrapper = document.querySelector('.slider__dots-wrapper');
-var dots = document.querySelectorAll('.slider__dot');
+(function() {
+  // слайдер Стихи и цитаты о войне
+  var slideIndex = 1;
+  var slides = document.querySelectorAll('.slider__item');
+  var prev = document.querySelector('.slider__button--prev');
+  var next = document.querySelector('.slider__button--next');
+  var sliderDotsWrapper = document.querySelector('.slider__dots-wrapper');
+  var dots = document.querySelectorAll('.slider__dot');
 
-var showSlides = function(n) {
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  };
-
-  for (var j = 0; j < dots.length; j++) {
-    dots[j].classList.remove('slider__dot--active');
-  };
-
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].classList.add('slider__dot--active');
-};
-
-showSlides(slideIndex);
-
-var plusSlides = function(n) {
-  showSlides(slideIndex += n)
-};
-
-var currentSlide = function(n) {
-  showSlides(slideIndex = n)
-};
-
-prev.addEventListener('click', function() {
-  plusSlides(-1);
-});
-
-next.addEventListener('click', function() {
-  plusSlides(1);
-});
-
-sliderDotsWrapper.addEventListener('click', function(evt) {
-  for (var i = 0; i < dots.length + 1; i++) {
-    if (evt.target.classList.contains('slider__dot') && evt.target === dots[i - 1]) {
-      currentSlide(i);
+  var showSlides = function(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
     }
-  }
-});
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    };
+
+    for (var j = 0; j < dots.length; j++) {
+      dots[j].classList.remove('slider__dot--active');
+    };
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('slider__dot--active');
+  };
+
+  showSlides(slideIndex);
+
+  var plusSlides = function(n) {
+    showSlides(slideIndex += n)
+  };
+
+  var currentSlide = function(n) {
+    showSlides(slideIndex = n)
+  };
+
+  prev.addEventListener('click', function() {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', function() {
+    plusSlides(1);
+  });
+
+  sliderDotsWrapper.addEventListener('click', function(evt) {
+    for (var i = 0; i < dots.length + 1; i++) {
+      if (evt.target.classList.contains('slider__dot') && evt.target === dots[i - 1]) {
+        currentSlide(i);
+      }
+    }
+  });
+
+})();
